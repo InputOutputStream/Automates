dc5931daa270e0c04eda0460a73c72facfef1687
ONAMBELE KONI <bryan.koni@facsciences-uy1.cm>
2025-06-21 19:28:08 +0100

    Ajout du code dominate

---
+++ projet/static/animation.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", () => {
+        alert("Traitement du mot en cours...");
+    });
+});
