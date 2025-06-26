f3d0c237cfc52dd020a055958fc8a243c34de9c6
ONAMBELE KONI <bryan.koni@facsciences-uy1.cm>
2025-06-26 12:05:50 +0100

    modification

--- Automate.py
+++ Automate.py
@@ -265,6 +265,7 @@ class AFDC(ADC):
         pass
 
 
+
 class AND(Automate):
     """
     Automate Non Déterministe.
@@ -273,10 +274,11 @@ class AND(Automate):
     spécifiques aux automates non déterministes.
     """
     
-    def __init__(self, alphabet: Set[str], etats: Set[str], etat_initial: str, 
-                 etats_finaux: Set[str]) -> None:
+    def __init__(self, alphabet: Set[str], etats: Set[str], etat_initial: str, etats_finaux: Set[str], Value: dict) -> None:
         """Initialise un AND."""
-        pass
+        super().__init__(alphabet,etats,etat_initial,etats_finaux)
+        self.Value = Value
+        
     
     def ajouter_transition(self, etat_source: str, symbole: str, etat_cible: str) -> None:
         """Ajoute une transition non déterministe."""
@@ -290,21 +292,121 @@ class AND(Automate):
         """Retourne un ensemble d'états (non déterminisme)."""
         pass
     
-    def reconnaitre_mot(self, mot: str) -> bool:
+    def reconnaitre_mot(self, mot: str,depart: int) -> bool:
         """Reconnaissance non déterministe d'un mot."""
-        pass
+        etat = [depart]
+        #print(f"Taille: {len(mot)}")
+        if mot == "" and depart in self.etat_finaux:
+            return True
+        elif mot == "" and depart not in self.etat_finaux:
+            return False
+
+        issus = [] #Cette liste permet de contoler l'évolution du mot dans le cas ou on se trouve sur un état avec plusieurs transitions sur le meme symbole
+    
+        for i in range(len(mot)):
+            symbole = mot[i]
+            try :
+                if self.Value[etat[-1]][symbole]:
+                    pass
+            except:
+                pass
+            else:
+                if isinstance(self.Value[etat[-1]][symbole],set) == False:
+                    etat.append(self.Value[etat[-1]][symbole])
+                else:
+                    print(f"Else: {self.Value[etat[-1]][symbole]}")
+                    for t in self.Value[etat[-1]][symbole]:
+                        issus.append(self.reconnaitre_mot(mot[i+1:],t))
+                        etat.append(t)
+        print(f"Issus: {issus}")
+        if True in issus:
+            return True
+                    
+        if etat[-1] in self.etat_finaux:
+            return True
+        return False
+    
+    
     
     def est_deterministe(self) -> bool:
         """Vérifie le déterminisme."""
-        pass
+        deterministe = True
+        for value in self.Value:
+            for sub_value in self.Value[value]:
+                if len(self.Value[value][sub_value]) != 1:
+                    return False
+        return deterministe
     
     def est_complet(self) -> bool:
         """Vérifie la complétude."""
+        
         pass
+    
+    #Add 
+    def get_tableau_transition(self) -> dict:
+        """
+            Fonction qui retourne le tableau de transitions d'un AND 
+        """
+        tableau_transition = {}
+        elements = {}
+        liste_etats = [self.etat_initial]
+        treated = []
+        while liste_etats != []:
+            etat = liste_etats[0]
+            elements = {}
+            for a in self.alphabet: 
+                elements.update({a:""})
+            
+            if isinstance(etat, str) == False:
+                for j in self.alphabet:
+                    if j in self.Value[etat]:
+                        if elements[j] == "":
+                            elements[j] = self.Value[etat][j]
+                        else:
+                            elements[j] = {elements[j],self.Value[etat][j]}
+                        tableau_transition.update({etat: elements})
+            else:
+                nouvel_etat = etat.split(".")
+                for t in nouvel_etat:
+                    for j in self.alphabet:
+                        if j in self.Value[int(t)]:
+                            if elements[j] == "":
+                                elements[j] = self.Value[int(t)][j]
+                            else:
+                                elements[j] = {elements[j],self.Value[int(t)][j]}
+                tableau_transition.update({etat: elements})
+            
+            for m in elements:
+                if (elements[m] not in liste_etats) and (elements[m] != ""):
+                    if isinstance(elements[m],set) == True:
+                        nouvel_etat = ""
+                        for value in elements[m]:
+                            nouvel_etat += "." + str(value)
+                        elements[m] = nouvel_etat[1:]
+                        if elements[m] not in treated:    
+                            liste_etats.append(elements[m])
+                    else:
+                        if elements[m] not in treated:    
+                            liste_etats.append(elements[m])
+            treated.append(etat)
+            liste_etats.pop(liste_etats.index(etat))
+            
+            
+            if liste_etats == []:
+                return tableau_transition
+                        
+        return tableau_transition
+        
+            
     
     def determiniser(self) -> ADC:
         """Convertit en automate déterministe équivalent."""
+        tableau_transition = self.get_tableau_transition()
+        
         pass
+        
+        
+        
     
     def afficher(self) -> str:
         """Affichage spécifique aux AND."""
